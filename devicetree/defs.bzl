bazel-contrib/rules_devicetree

commit 92495d8d6d8f4f7e9d9576ca420340d8c2ef16c1

Add a test on load visibility for //private bzl.
The default load visibility is public, which is not suitable for
//private. This test ensures that all our .bzl files under //private
must have a visibility() statement.

Caveat: This does not include sub-packages below private.

fixes #2

devicetree/defs.bzl
@@ -1,0 +1,5 @@
+"Public API re-exports"
+
+def example():
+    """This is an example"""
+    pass

MODULE.bazel
@@ -38,6 +38,7 @@ bazel_dep(name = "gazelle", version = "0.35.0", dev_dependency = True, repo_name
 bazel_dep(name = "bazel_skylib_gazelle_plugin", version = "1.4.1", dev_dependency = True)
 bazel_dep(name = "aspect_bazel_lib", version = "1.32.1", dev_dependency = True)
 bazel_dep(name = "buildifier_prebuilt", version = "6.1.2", dev_dependency = True)
+bazel_dep(name = "rules_python", version = "1.5.0", dev_dependency = True)
 
 autodetected_toolchain_repo = use_repo_rule("//devicetree/private:autodetected_toolchain_repo.bzl", "autodetected_toolchain_repo")
 

devicetree/private/BUILD.bazel
@@ -35,3 +35,9 @@ bzl_library(
         ":devicetree_toolchain_info",
     ],
 )
+
+filegroup(
+    name = "all_bzl_files",
+    srcs = glob(["*.bzl"]),
+    visibility = ["//devicetree/tests:__subpackages__"],
+)

devicetree/tests/load_visibility_test/BUILD.bazel
@@ -1,0 +1,22 @@
+# Copyright (C) 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+load("@rules_python//python:defs.bzl", "py_test")
+
+py_test(
+    name = "load_visibility_test",
+    srcs = ["load_visibility_test.py"],
+    args = ["$(rootpaths //devicetree/private:all_bzl_files)"],
+    data = ["//devicetree/private:all_bzl_files"],
+)

devicetree/tests/load_visibility_test/load_visibility_test.py
@@ -1,0 +1,48 @@
+# Copyright (C) 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import logging
+import unittest
+import pathlib
+import sys
+
+arguments: argparse.Namespace
+
+
+def load_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bzl_files", nargs="*", type=pathlib.Path)
+    args, unknown = parser.parse_known_args()
+    return args, unknown
+
+
+class BzlLoadVisibilityTest(unittest.TestCase):
+    def test_bzl_files_have_load_visibility(self):
+        for bzl_file in arguments.bzl_files:
+            with self.subTest(bzl_file=bzl_file):
+                with bzl_file.open() as f:
+                    has_visibility = any([line.startswith("visibility(")
+                                          for line in f.readlines()])
+                    self.assertTrue(has_visibility,
+                                    f"File {bzl_file} does not have visibility")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s: %(message)s"
+    )
+    arguments, unknown = load_arguments()
+    sys.argv[1:] = unknown
+    unittest.main()

commit 3d3559d56e40ca4c01618e6d639c04b7f20e8ed7

fix(rules): remove defs.bzl again
This was accidentally re-added by 92495d8d6d8f. Re-delete the file
again.

Fixes: 92495d8d6d8f ("Add a test on load visibility for //private bzl.")

devicetree/defs.bzl
@@ -1,5 +1,0 @@
-"Public API re-exports"
-
-def example():
-    """This is an example"""
-    pass